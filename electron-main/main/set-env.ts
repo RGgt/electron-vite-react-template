4adca72cc1d01a08cc5494dd1c80dc7a6321c7ae
RGgt <rggt.inbox@gmail.com>
2023-08-24 11:41:07 +0300

    folder renamed: public => electron-resources

--- electron-main/main/set-env.ts
+++ electron-main/main/set-env.ts
@@ -4,7 +4,7 @@ import path from 'path';
 function setEnv() {
   const FROM_HERE_TO_ROOT = '../../'; // PARENT OF DIST-ELECTRON, DIST-RENDERER and ELECTRON_RESOURCES
   const FOLDER_NAME__DIST_RENDERER = 'dist-renderer';
-  const FOLDER_NAME__ELECTRON_RESOURCES = 'public';
+  const FOLDER_NAME__ELECTRON_RESOURCES = 'electron-resources';
   const FOLDER_NAME__PROJECT_ROOT = path.join(__dirname, FROM_HERE_TO_ROOT);
   process.env.DIST = path.join(
     FOLDER_NAME__PROJECT_ROOT,
