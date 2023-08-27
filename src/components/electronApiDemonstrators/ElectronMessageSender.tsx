8abc1469e92bae3292fafc2d1d4a3b49f15e2f8d
RGgt <rggt.inbox@gmail.com>
2023-08-27 12:16:43 +0300

    Electron now replies to messages send. plus, added some css for demo code

--- src/components/electronApiDemonstrators/ElectronMessageSender.tsx
+++ src/components/electronApiDemonstrators/ElectronMessageSender.tsx
@@ -1,16 +1,20 @@
+import './ElectronMessageSender.css';
 import { useState } from 'react';
 
 function ElectronMessageSender() {
   const [messageToElectron, setMessageToElectron] = useState('Hi, Electron!');
+  const [responseFromElectron, setResponseFromElectron] = useState('');
 
   const onClickOnTestSendMessage = async () => {
     const response =
       await window.electronAPI.sendTextMessage(messageToElectron);
     console.log(response);
+    setResponseFromElectron(response);
   };
 
   return (
-    <div>
+    <div className="messageDemonstrator">
+      <h2>Sending text messages to Electron</h2>
       <label>
         Your message to Electron:
         <input
@@ -22,6 +26,13 @@ function ElectronMessageSender() {
       <button onClick={onClickOnTestSendMessage}>
         Send message to Electron
       </button>
+      <br />
+      {responseFromElectron && responseFromElectron.trim() !== '' && (
+        <label>
+          Electron's reply:
+          <blockquote>{responseFromElectron} </blockquote>
+        </label>
+      )}
     </div>
   );
 }
